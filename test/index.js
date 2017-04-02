77b76d19cc3d7a03de9868ab323e9a10e7704e37
Denis Kalinichenko <d.kalinichenkooo@gmail.com>
2017-04-03 01:46:19 +0200

    messages and rooms

--- test/index.js
+++ test/index.js
@@ -1,11 +1,12 @@
 import http from 'http';
 import { expect } from 'chai';
 
+import config from "./../lib/config";
 import '../lib/index.js';
 
 describe('Node Server', () => {
     it('should return 200', done => {
-        http.get('http://127.0.0.1:1337', res => {
+        http.get('http://127.0.0.1:' + config.port, res => {
             expect(res.statusCode).to.equal(200);
             done();
         });
